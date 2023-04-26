cd9180b37a3ffefde52234232c6ed2ab0a647df8
DarpanPtl <darpankpatel007@gmail.com>
2023-04-26 11:32:45 +0530

    inotebook

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+/*mongoose
+  .connect("mongodb://127.0.0.1:27017/login", {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connect");
+  })
+  .catch(() => {
+    console.log("not connect");
+  });
+
+module.exports = mongoose;*/
+
+//const mongoURL = "mongodb://127.0.0.1:27017/login";
+const connectToMongo = async () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/login", { useNewUrlParser: true })
+    .then(() => {
+      console.log("Connected to database");
+    })
+    .catch((err) => {
+      console.error("Error connecting to database", err);
+    });
+};
+
+module.exports = connectToMongo;
---
+++ index.js
@@ -0,0 +1,28 @@
+// require("./db");
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port https://localhost:${port}`);
+// });
+
+const connectToMongo = require("./db");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+connectToMongo();
